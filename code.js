d8e6b840394b4f689be81f665d9b763d64c6dfe7
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-02-26 15:09:00 +0530

    Refactor button state handling

--- code.js
+++ code.js
@@ -480,7 +480,8 @@ function createButton(primaryColor, secondaryColor, buttonRadius, fontStyle, but
             button.fillStyleId = hoverColorStyle.id;
         }
         else if (buttonState == 'pressed') {
-            button.fills = [{ type: 'SOLID', color: { r: Math.max(0, primaryColor.r - 0.2), g: Math.max(0, primaryColor.g - 0.2), b: Math.max(0, primaryColor.b - 0.2) } }];
+            button.fillStyleId = pressedColorStyle.id;
+            // button.fills = [{ type: 'SOLID', color: { r: Math.max(0, primaryColor.r - 0.2), g: Math.max(0, primaryColor.g - 0.2), b: Math.max(0, primaryColor.b - 0.2) } }];
         }
         else if (buttonState == 'disabled') {
             button.fills = [{
